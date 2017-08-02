066363d3f7057afcd51ca910e304a15e86b92367
Shivansh Mathur <mathurshiva90@outlook.com>
2017-08-03 00:55:11 +0530

    added python folder

--- Code/Hello Window.cpp
+++ Code/Hello Window.cpp
@@ -45,7 +45,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) 
 		EndPaint(hwnd, &ps);
 		return 0;
 	case WM_DESTROY:
-		PostQuitMessage(0);
+	//	PostQuitMessage(0);
 		return 0;
 	}return DefWindowProc(hwnd, message, wParam, lParam);
 }
