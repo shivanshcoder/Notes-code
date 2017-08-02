c96b4bdc95e3b471a834740d940dcd0165922299
Shivansh Mathur <mathurshiva90@outlook.com>
2017-08-03 00:46:55 +0530

    Hello Window

--- Code/Hello Window.cpp
+++ Code/Hello Window.cpp
@@ -12,7 +12,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine,
 	wndclass.cbWndExtra = 0;
 	wndclass.style = CS_HREDRAW | CS_VREDRAW;
 	wndclass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
-	wndclass.hCursor = LoadIcon(NULL, IDI_APPLICATION);
+	wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
 	wndclass.lpfnWndProc = WndProc;
 	wndclass.lpszClassName = szAppName;
 	wndclass.lpszMenuName = NULL;
@@ -26,7 +27,25 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine,
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
 	}
-
+	return msg.wParam;
 }
 
 
+LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
+	HDC hdc;
+	PAINTSTRUCT ps;
+	RECT rect;
+	switch (message) {
+	case WM_CREATE:
+		return 0;
+	case WM_PAINT:
+		hdc = BeginPaint(hwnd, &ps);
+		GetClientRect(hwnd, &rect);
+		DrawText(hdc, TEXT("Hello World"), -1, &rect,DT_SINGLELINE|DT_CENTER|DT_VCENTER);
+		EndPaint(hwnd, &ps);
+		return 0;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}return DefWindowProc(hwnd, message, wParam, lParam);
+}
